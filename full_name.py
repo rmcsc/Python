488d2ed9255b95b7277e166865f5c4a28cdee731
Raúl <rmcsc@users.noreply.github.com>
2022-04-27 18:09:02 -0400

    Added code for Python 3.5 or earlier

--- full_name.py
+++ full_name.py
@@ -1,6 +1,10 @@
 first_name = "ada"
 last_name = "lovelace"
+
+# for Python 3.6+
 full_name = f"{first_name} {last_name}"
+# for Python 3.5 or earlier
+# full_name = "{} {}".format(first_name, last_name)
 
 message = f"Hello, {full_name.title()}!"
 print(message)
