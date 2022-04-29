4e52ed5b52c74508521146415f7e971e4a041c77
Raúl <rmcsc@users.noreply.github.com>
2022-04-28 21:59:44 -0400

    Ended chapter 3, starting chapter 4

---
+++ Chapter 3/intentional_error.py
@@ -0,0 +1,12 @@
+# Try It Yourself 3-11
+# Making an index error on purpose, then correcting the error
+
+# This is the error:
+# |-------------------------|
+# | motorcycles = []        |
+# | print(motorcycles[-1])  |
+# |-------------------------|
+
+# This is the code, corrected:
+motorcycles = ['honda', 'suzuki', 'bmw']
+print(motorcycles[-1])
