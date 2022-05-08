0e0044015852fc14e95cc6a54080a66f9156a4e6
Raúl <rmcsc@users.noreply.github.com>
2022-05-08 01:33:44 -0400

    Last exercise of the night; ch 5 again

---
+++ Chapter 5: if Statements/favorite_fruit.py
@@ -0,0 +1,18 @@
+# Try It Yourself 5-7: Favorite Fruit
+# Make a list of my three favorite fruits and call it favorite_fruits.
+# Write five if statements, checking whether a certain kind of fruit is
+# in my list. If it is, the iff block should print a statement like "You
+# really like bananas!"
+
+favorite_fruits = ['persimmon', 'banana', 'pear']
+
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+if 'pear' in favorite_fruits:
+    print("You really like pears!")
+if 'persimmon' in favorite_fruits:
+    print("You really like persimmons!")
+if 'orange' in favorite_fruits:
+    print("You really like oranges!")
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
--- Chapter 5: if Statements/stages_of_life.py
+++ Chapter 5: if Statements/stages_of_life.py
@@ -1,14 +1,22 @@
 # Try It Yourself 5-6: Stages of Life
 # Write an if-elif-else chain that determines a person's stage of life.
 # Set a value to age, then classify: if less than 2 years old, print
-# a message that the person is a baby; at least 2, but less than 1, that
+# a message that the person is a baby; at least 2, but less than 4, that
 # the person is a toddler; at least 4, but less than 13, that the person
 # is a kid; at least 13 but less than 20, a teenager; at least 20 but
 # less than 65, an adult; 65 or older, an elder.
 
-age = 16
+age = 13
 
 if age < 2:
     print("The person is a baby.")
-elif age <= 2 and age < 4:
+elif age >= 2 and age < 4:
     print("The person is a toddler.")
+elif age >= 4 and age < 13:
+    print("The person is a kid.")
+elif age >= 13 and age < 20:
+    print("The person is a teenager.")
+elif age >= 20 and age < 65:
+    print("The person is an adult.")
+else:
+    print("The person is an elder.")
